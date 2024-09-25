0cb6496dc88627bc49bbf19fb56ae29b2efbf8b2
Rizwan <95566592+rizu1512@users.noreply.github.com>
2024-09-25 23:50:08 +0500

    Update main.dart
    qestion 3 done

--- main.dart
+++ main.dart
@@ -1,17 +1,37 @@
+import 'dart:math';
 //: Create a list of names and print all names using list.
 void main(){
-    //print('Question No 1');
+    //print('Question No 1: Q.1: Create a list of names and print all names using list.');
     // List names =[
     //     'rizwan','hassan','Mehtab','Akhtar','Arif','sadiq'
     //     ];
     //     for(String name in names){
     //     print(name);
     //     }
-    // print('Question No 2');
+    // print('Question No 2: Create an empty list of type string called days. Use the add method to add names of 7 days and print all days.');
     // List days = [];
     // print(days);
     // days.addAll(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
     // print(days);
-    print('Question No 3');
-    List days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//     print('Question No 3: Create a list of Days and remove one by one from the end of list.');
+//     List days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+//     print(days);
+//     days.removeLast();
+//     print(days);
+//     days.removeLast();
+//     print(days);
+//     days.removeLast();
+//     print(days);
+//     days.removeLast();
+//     print(days);
+//     days.removeLast();
+//     print(days);
+//     days.removeLast();
+//     print(days);
+//     days.removeLast();
+//     print(days);
+
+// print('Question No 4:Create a list of numbers & write a program to get the smallest & greatest number from a list.');
+//   List number =[1,3,6,8,3,6,1,0,67];
+//   print('${number.reduce(max)}');
 }
