1fe7150d44e478a54cfa4311c8f1c43a3535d54f
Rizwan <95566592+rizu1512@users.noreply.github.com>
2024-09-25 20:43:41 +0500

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,17 @@
-
+//: Create a list of names and print all names using list.
+void main(){
+    //print('Question No 1');
+    // List names =[
+    //     'rizwan','hassan','Mehtab','Akhtar','Arif','sadiq'
+    //     ];
+    //     for(String name in names){
+    //     print(name);
+    //     }
+    // print('Question No 2');
+    // List days = [];
+    // print(days);
+    // days.addAll(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']);
+    // print(days);
+    print('Question No 3');
+    List days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+}
